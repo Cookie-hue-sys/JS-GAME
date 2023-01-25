8fb40a1e3d6249b14f1e91953b8bba7f20aa751b
Keilah <sapphirebluekk04@gmail.com>
2023-01-25 05:30:25 +0300

    js-path.js

--- js-path.js
+++ js-path.js
@@ -1,3 +1,4 @@
+
 let playerScore = 0;
 let compScore =0;
 let roundNum;
@@ -5,112 +6,104 @@ let roundNum;
 function getComputerChoice()
 {
      let choices = ["rock", "paper", "scissors"];
-     return choices[Math.floor(Math.random() * choices.length)];
+     return choices[Math.floor(Math.random() * choices.length)]; // picks a random index from the array length and then gives the value assigned to that index.
 }
+//console.log(getComputerChoice());
 
 
 
 function playRound(computerSelection, playerSelection)
 {
+   
 
-    let result = " ";
-    computerSelection = getComputerChoice();
-    playerSelection = playerSelection.toLowerCase();
-
-    for(roundNum =0; roundNum < 5; roundNum++)
+    if(computerSelection === "rock")
     {
-        playerSelection = prompt("Make your choice:")
-        computerSelection;
-
-        if(computerSelection == "rock")
+        if(playerSelection === "paper")
         {
-            if(playerSelection == "paper")
-            {
-                playerScore+=1;
-                result = ("You win. paper beats rock." + 
-                 "<br><br>player Score: " + playerScore + "<br><br> computer Score: " + compScore);
-            }
-            else if(playerSelection == "scissors")
-            {
-                compScore+=1;
-                result = ("You lose. rock beats scissors" + 
-                 "<br><br>player Score: " + playerScore + "<br><br> computer Score: " + compScore);
-            }
-            else
-            {
-                result = "Tie. You both chose rock."
-                result = "<br><br>Player Score: " + playerScore  + "<br><br>computer Score: " + compScore;
-            }
-
+            playerScore+=1;
+            return("You win. paper beats rock." + 
+             "\nplayer Score: " + playerScore + "\n computer Score: " + compScore);
         }
-
-        if(computerSelection == "scissors")
+        else if(playerSelection === "scissors")
         {
-            if(playerSelection == "rock")
-            {
-                playerScore+=1;
-                result = ("You win. rock beats scissors." + 
-                 "<br><br>player Score: " + playerScore + "<br><br> computer Score: " + compScore);
-            }
-            else if(playerSelection == "paper")
-            {
-                compScore+=1;
-                result = ("You lose. scissors beats paper" + 
-                 "<br><br>player Score: " + playerScore + "<br><br> computer Score: " + compScore);
-            }
-            else
-            {
-                result = "Tie. You both chose scissors."
-                result = "<br><br>Player Score: " + playerScore  + "<br><br>computer Score: " + compScore;
-            }
-
-        }
-
-        if(computerSelection == "paper")
-        {
-            if(playerSelection == "scissors")
-            {
-                playerScore+=1;
-                result = ("You win. scissors beats paper." + 
-                 "<br><br>player Score: " + playerScore + "<br><br> computer Score: " + compScore);
-            }
-            else if(playerSelection == "rock")
-            {
-                compScore+=1;
-                result = ("You lose. paper beats rock" + 
-                 "<br><br>player Score: " + playerScore + "<br><br> computer Score: " + compScore);
-            }
-            else
-            {
-                result = "Tie. You both chose paper."
-                result = "<br><br>Player Score: " + playerScore  + "<br><br>computer Score: " + compScore;
-            }
-
-        }
-          
-
-    }
-
-    if (roundNum == 5)
-    {
-        if(compScore > playerScore)
-        {
-            result = ("You lost the game. Better luck next time.");
-        }
-        else if (compScore < playerScore)
-        {
-            result = (" You won the game. Congratulations!");
+            compScore+=1;
+            return("You lose. rock beats scissors" + 
+             "\nplayer Score: " + playerScore + "\n computer Score: " + compScore);
         }
         else
         {
-            result = ("No winner. Both you and the computer tie.")
+            return("Tie. You both chose rock.\nPlayer Score: " + playerScore  + "\ncomputer Score: " + compScore);
         }
+
     }
 
+    if(computerSelection == "scissors")
+    {
+        if(playerSelection == "rock")
+        {
+            playerScore+=1;
+            return("You win. rock beats scissors." + 
+             "\nplayer Score: " + playerScore + "\n computer Score: " + compScore);
+        }
+        else if(playerSelection == "paper")
+        {
+            compScore+=1;
+            return("You lose. scissors beats paper" + 
+             "\nplayer Score: " + playerScore + "\n computer Score: " + compScore);
+        }
+        else
+        {
+            return( "Tie. You both chose scissors. \nPlayer Score: " + playerScore  + "\ncomputer Score: " + compScore);
+        }
 
+    }
 
-   
-  
+    if(computerSelection === "paper")
+    {
+        if(playerSelection == "scissors")
+        {
+            playerScore+=1;
+            return("You win. scissors beats paper." + 
+             "\nplayer Score: " + playerScore + "\n computer Score: " + compScore);
+        }
+        else if(playerSelection == "rock")
+        {
+            compScore+=1;
+            return("You lose. paper beats rock" + 
+             "\nplayer Score: " + playerScore + "\n computer Score: " + compScore);
+        }
+        else
+        {
+            return "Tie. You both chose paper. \nPlayer Score: " + playerScore  + "\ncomputer Score: " + compScore;
+        }
 
 }
+} //playerSelection = prompt("Make your choice:")
+const computerSelection = getComputerChoice(); //calls the getComputerChoice function to know the value the computer has chosen
+//console.log(playRound(playerSelection, computerSelection));
 
+function game(){
+    for(roundNum =1; roundNum <= 5; roundNum++) //plays the game for 5 rounds
+    {
+        playerSelection = prompt("Make your choice:")
+        playRound();
+        console.log(playRound(playerSelection, computerSelection));
+
+        if (roundNum == 5) // looks at the end of the 5 rounds to determine overall winner.
+    {
+        if(compScore > playerScore)
+        {
+            return "You lost the game. Better luck next time.";
+        }
+        else if (compScore < playerScore)
+        {
+            return " You won the game. Congratulations!";
+        }
+        else
+        {
+            return "No winner. Both you and the computer tie.";
+        }
+    }
+    }
+}
+console.log(game());
